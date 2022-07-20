6aedb9cb4db2b6a49c3bd22aba853d68d00278b3
FranciscoG001 <82593913+FranciscoG001@users.noreply.github.com>
2022-07-20 12:33:17 +0100

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -8,7 +8,3 @@ class TestTextColor(unittest.TestCase):
         """Test"""
         resultcolor = color
         self.assertEqual(resultcolor, (255, 0, 0))
-
-if __name__ == "__main__":
-    test = TestTextColor()
-    test.testtextcolor()
